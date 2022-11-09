a7969d427b7e0d22625794e1b3f40b86edb1d5d2
minhazk <minhazulkarim26@gmail.com>
2022-11-09 11:55:39 +0000

    ui fix

--- src/screens/Create.js
+++ src/screens/Create.js
@@ -1,18 +1,25 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ScrollView, View } from 'react-native';
 import CustomButton from '../components/CustomButton';
 import FormInput from '../components/FormInput';
 import { useGameContext } from '../context/GameContext';
 import { screen } from '../styles/global';
 
+const DEFAULT_SETTINGS = {
+    competition: '',
+    date: new Date().toLocaleDateString('en-GB', { day: 'numeric', month: 'long', year: 'numeric' }),
+    rink: '',
+    team1: '',
+    team2: '',
+};
+
 const Create = ({ navigation }) => {
-    const [gameInfo, setGameInfo] = useState({
-        competition: '',
-        date: new Date().toLocaleDateString('en-GB', { day: 'numeric', month: 'long', year: 'numeric' }),
-        team1: null,
-        team2: null,
-    });
-    const { createGame } = useGameContext();
+    const [gameInfo, setGameInfo] = useState(DEFAULT_SETTINGS);
+    const { createGame, games } = useGameContext();
+
+    useEffect(() => {
+        setGameInfo(DEFAULT_SETTINGS);
+    }, [games]);
 
     const updateGameInfo = (key, value) => {
         setGameInfo(prev => {
@@ -40,11 +47,11 @@ const Create = ({ navigation }) => {
     return (
         <ScrollView style={screen.page}>
             <View style={screen.topGap}>
-                <FormInput label='Competition' onChange={text => updateGameInfo('competition', text)} />
-                <FormInput label='Rink No' onChange={text => updateGameInfo('rink', text)} keyboardType='numeric' />
+                <FormInput label='Competition' onChange={text => updateGameInfo('competition', text)} defaultValue={gameInfo.competition} />
+                <FormInput label='Rink No' onChange={text => updateGameInfo('rink', text)} keyboardType='numeric' defaultValue={gameInfo.rink} />
                 <FormInput label='Date' onChange={null} disabled={false} defaultValue={gameInfo.date} />
-                <FormInput label='Team One Name' onChange={text => updateGameInfo('team1', text)} />
-                <FormInput label='Team Two Name' onChange={text => updateGameInfo('team2', text)} />
+                <FormInput label='Team One Name' onChange={text => updateGameInfo('team1', text)} defaultValue={gameInfo.team1} />
+                <FormInput label='Team Two Name' onChange={text => updateGameInfo('team2', text)} defaultValue={gameInfo.team2} />
                 <CustomButton label='Create Game' onPress={handleCreateGame} />
             </View>
         </ScrollView>
